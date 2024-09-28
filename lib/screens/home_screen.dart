891635f5f472b5c6c82d0f3d8b7ac97d4a7c1cf3
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-09-28 22:59:54 +0200

    HomeScreen: moved to DB data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,17 @@
-import 'dart:convert';
+import 'dart:developer' as dev;
 import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wt_wiki/AppLocalisations.dart';
+import 'package:flutter_wt_wiki/classes/synthetic_vehicle.dart';
 import 'package:flutter_wt_wiki/constants.dart';
+import 'package:flutter_wt_wiki/database_helper.dart';
 import 'package:flutter_wt_wiki/screens/vehicle_screen.dart';
 import 'package:flutter_wt_wiki/widgets/homedrawer.dart';
-import 'package:http/http.dart' as http;
+
+import '../classes/vehicle.dart';
+import '../utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,173 +22,129 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenState extends State<HomeScreen> {
   final Map<String, List<String>> _typesByCat = {
-    'Aviation': Constants.AVIATION_VEHICLE_TYPES,
-    'Ground': Constants.GROUND_VEHICLE_TYPES,
-    'Fleet': Constants.FLEET_VEHICLE_TYPES,
+    'Aviation': Constants.aviationVehicleTypes,
+    'Ground': Constants.groundVehicleTypes,
+    'Fleet': Constants.fleetVehicleTypes,
   };
 
   int _selectedCategory = 0;
 
-  Future<List<dynamic>> _fetchData(String type) async {
-    final response = await http.get(Uri.parse('https://wtvehiclesapi.sgambe.serv00.net/api/vehicles?type=$type'));
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Failed to load chart data');
-    }
-  }
+  Future<List<dynamic>> _fetchData(List<String> type) async {
+    List<String> whereClauses = [];
+    List<dynamic> whereArgs = [];
 
-  String _intToRoman(int num) {
-    switch (num) {
-      case 1:
-        return 'I';
-      case 2:
-        return 'II';
-      case 3:
-        return 'III';
-      case 4:
-        return 'IV';
-      case 5:
-        return 'V';
-      case 6:
-        return 'VI';
-      case 7:
-        return 'VII';
-      case 8:
-        return 'VIII';
-      case 9:
-        return 'IX';
-      case 10:
-        return 'X';
-      default:
-        return num.toString();
+    if (type.isNotEmpty) {
+      whereClauses.add("vehicle_type IN (${List.filled(type.length, "?").join(',')})");
+      type.forEach((element) {
+        whereArgs.add(element);
+      });
+    }
+
+    String whereClause = whereClauses.isNotEmpty ? whereClauses.join(' AND ') : "";
+
+    try {
+      List<SyntheticVehicle> dbVehicles = await DatabaseHelper().getSyntheticVehiclesByFilters("vehicle", whereClause, whereArgs);
+      if (dbVehicles.isEmpty) {
+        dev.log("No vehicles found in the database for the given query: $whereClause with args: $whereArgs");
+        return [];
+      }
+      return dbVehicles;
+    } catch (e) {
+      dev.log("Error querying the database: $e");
+      return [];
     }
   }
 
   @override
   Widget build(BuildContext context) {
     String selectedType = _typesByCat.keys.elementAt(_selectedCategory);
-
-    String filterType = _typesByCat[selectedType]!.asMap().values.join(',');
+    List<String>? filterTypes = _typesByCat[selectedType];
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () => Navigator.of(context).pushNamed('/search'),
-          ),
-          IconButton(icon: const Icon(Icons.filter_list_sharp), onPressed: () async {})
-        ],
-      ),
-      drawer: const HomeDrawer(),
-      body: FutureBuilder<List<dynamic>>(
-          future: _fetchData(filterType),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else {
-              double screenWidth = MediaQuery.of(context).size.width;
-              int crossAxisCount = max((screenWidth / 200).floor(), 2);
-              return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: crossAxisCount,
-                  ),
-                  shrinkWrap: true,
-                  addAutomaticKeepAlives: true,
-                  itemCount: snapshot.data!.length ?? 0,
-                  itemBuilder: (context, index) {
-                    final item = snapshot.data![index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => VehicleScreen(
-                              vehicleIdentifier: item['identifier'],
-                            ),
-                          ),
-                        );
-                      },
-                      child: GridTile(
-                        header: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                          child: Text(
-                            item['identifier'] != null
-                                ? AppLocalizations.of(context).stringBy('vehicles', (item['identifier'] + "_short").toLowerCase())
-                                : item['identifier'],
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        footer: Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.asset(
-                                Constants.COUNTRY_TO_FLAG_MAP[item['country']]!,
-                                width: 40,
-                                height: 40,
+        appBar: AppBar(
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () => Navigator.of(context).pushNamed('/search'),
+            ),
+            IconButton(icon: const Icon(Icons.filter_list_sharp), onPressed: () async {})
+          ],
+        ),
+        drawer: const HomeDrawer(),
+        body: FutureBuilder<List<dynamic>>(
+            future: _fetchData(filterTypes!),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                double screenWidth = MediaQuery.of(context).size.width;
+                int crossAxisCount = max((screenWidth / 200).floor(), 2);
+                return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: crossAxisCount),
+                    shrinkWrap: true,
+                    addAutomaticKeepAlives: true,
+                    itemCount: snapshot.data!.length ?? 0,
+                    itemBuilder: (context, index) {
+                      final SyntheticVehicle dbVehicle = snapshot.data![index];
+                      return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => VehicleScreen(vehicleIdentifier: dbVehicle.identifier!)),
+                            );
+                          },
+                          child: GridTile(
+                              header: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                                child: Text(dbVehicle.identifier != null ? AppLocalizations.of(context).stringBy('vehicles', ("${dbVehicle.identifier!}_short").toLowerCase()) : dbVehicle.identifier!,
+                                    textAlign: TextAlign.center),
                               ),
-                              Text(
-                                "Rank ${_intToRoman(item['era'])} ${Constants.VEHICLE_TYPE_TO_ICON[item['vehicle_type']] ?? ''}",
-                                textAlign: TextAlign.right,
-                                style: const TextStyle(fontFamily: 'CustomFont'),
+                              footer: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Image.asset(Constants.countryToFlagMap[dbVehicle.country]!, width: 40, height: 40),
+                                    Text("Rank ${intToRoman(dbVehicle.era)} ${Constants.vehicleTypeToIcon[dbVehicle.vehicleType] ?? ''}",
+                                        textAlign: TextAlign.right, style: const TextStyle(fontFamily: 'CustomFont')),
+                                  ],
+                                ),
                               ),
-                            ],
-                          ),
-                        ),
-                        child: Card(
-                            child: CachedNetworkImage(
-                                imageUrl: item['images']['image'],
-                                imageBuilder: (context, imageProvider) => Image(
-                                    image: imageProvider,
-                                    loadingBuilder: (context, child, loadingProgress) {
-                                      if (loadingProgress == null) {
-                                        return child;
-                                      } else {
-                                        return const Center(
-                                          child: CircularProgressIndicator(),
-                                        );
-                                      }
-                                    }),
-                                errorWidget: (context, url, error) => Image.asset('assets/images/uknown.png'))),
-                      ),
-                    );
-                  });
-            }
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //Shuffle the loaded data
-          setState(() {});
-        },
-        child: const Icon(Icons.shuffle),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedCategory,
-        onTap: (int index) {
-          setState(() {
-            _selectedCategory = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.flight_takeoff),
-            label: 'Aviation',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_car),
-            label: 'Ground',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.directions_ferry),
-            label: 'Fleet',
-          ),
-        ],
-      ),
-    );
+                              child: Card(
+                                  child: CachedNetworkImage(
+                                      imageUrl: "https://wtvehiclesapi.sgambe.serv00.net/assets/images/${dbVehicle.identifier!.toLowerCase()}.png",
+                                      imageBuilder: (context, imageProvider) => Image(
+                                          image: imageProvider,
+                                          loadingBuilder: (context, child, loadingProgress) {
+                                            if (loadingProgress == null) {
+                                              return child;
+                                            } else {
+                                              return const Center(child: CircularProgressIndicator());
+                                            }
+                                          }),
+                                      errorWidget: (context, url, error) => Image.asset('assets/images/uknown.png')))));
+                    });
+              }
+            }),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //TODO: Implement shuffle
+            },
+            child: const Icon(Icons.shuffle)),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedCategory,
+          onTap: (int index) {
+            setState(() {
+              _selectedCategory = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.flight_takeoff), label: 'Aviation'),
+            BottomNavigationBarItem(icon: Icon(Icons.directions_car), label: 'Ground'),
+            BottomNavigationBarItem(icon: Icon(Icons.directions_ferry), label: 'Fleet')
+          ],
+        ));
   }
 }
